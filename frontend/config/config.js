b39c02f36dd43fc98cd2f5e5efe024abaf4a2dd2
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2025-02-16 21:52:42 +0300

    updated

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://brandspark-4.onrender.com";
-import { API_BASE_URL } from "./config";
+// import { API_BASE_URL } from "./config";
 
 fetch(`${API_BASE_URL}/api/data`)
   .then(response => response.json())
